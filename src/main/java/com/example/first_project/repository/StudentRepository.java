ac311580260a72b7e56a4732ee52e23fd4a666bc
YohanNayanajith <yohannayanajith40@gmail.com>
2022-06-09 00:27:30 +0530

    Second Commit

--- src/main/java/com/example/first_project/repository/StudentRepository.java
+++ src/main/java/com/example/first_project/repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.example.first_project.repository;
 
-public interface StudentRepository {
+import com.example.first_project.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Integer> {
 }
